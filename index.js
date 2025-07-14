b5d7288d23fd3661bf04fad5d7563fabfac78a56
piarda <matt.rindini@gmail.com>
2025-07-14 17:59:08 -0400

    Global scope variables declared, and function-scoped and blocked-scoped variables created

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+// both are global scopes, outside of any function
+
+function addBurger() {
+  var newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+// var makes this a function-scope variable
+
+if(true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake';
+    }
+}
+// block-scoped variable and function is declared inside the block
